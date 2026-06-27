79f325570c27cdaa0334ee85121f637d67f28227
bluecitylights <bluecitylights>
2026-06-27 20:45:39 +0000

    fix moutns, install superpowers

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "🚀 Devcontainer is opgestart. Antigravity CLI controleren..."
+# echo "🚀 Devcontainer is opgestart. Antigravity CLI controleren..."
 
-# Controleer of we ingelogd zijn, zo niet, geef een melding
-if agy init --check; then
-    echo "✅ Antigravity is klaar voor gebruik!"
-else
-    echo "⚠️ Je bent nog niet ingelogd in agy. Typ straks 'agy auth login' in je terminal!"
-fi
+# # Controleer of we ingelogd zijn, zo niet, geef een melding
+# if agy init --check; then
+#     echo "✅ Antigravity is klaar voor gebruik!"
+# else
+#     echo "⚠️ Je bent nog niet ingelogd in agy. Typ straks 'agy auth login' in je terminal!"
+# fi
+
+agy plugin install https://github.com/obra/superpowers
